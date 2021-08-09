4eb7aa1c861646408d4d9d6ce735f232536d5c91
Sam Bassong <sam.bassong@gmail.com>
2021-08-09 13:58:29 -0700

    added untested startsFrom method and associated variables

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
+//global variables
+const redStartPoint = document.querySelector('#c1')
+const greenStartPoint = document.querySelector('#c14')
+const yellowStartPoint = document.querySelector('#c27')
+const blueStartPoint = document.querySelector('#c40')
+let cell = document.querySelector(`.${}${}`)
+let startsFrom = ''
+
+
 //Die functionality
 // const dieDiv = document.querySelector('.die')
-const throwDie = () => {
-  return Math.ceil(Math.random() * 6)
-} 
+const throwDie = () => Math.ceil(Math.random() * 6)
+let dieVal = 0
 
+
+//pawn class and methods/behavior
+class Pawn  {
+  constructor (color) {
+    this.color = color
+  }
+
+  startsFrom(target) {
+    if (target.id.parentElement.id.StartsWith('red-')) {
+      target.parentElement.removeChild(target)
+      redStartingPoint.appendChild(target)
+    } else if (target.id.parentElement.id.StartsWith('green-')) {
+      target.parentElement.removeChild(target)
+      greenStartingPoint.appendChild(target)
+    } else if (target.id.parentElement.id.StartsWith('yellow-')) {
+      target.parentElement.removeChild(target)
+      yellowStartingPoint.appendChild(target)
+    } else {
+      target.parentElement.removeChild(target)
+      blueStartingPoint.appendChild(target)
+    }
+  }
+
+  movePawn(e) {
+    let curPawn = e.target
+    if (curPawn.parentElement.id.startsWith())
+    startsFrom
+    
+    curPawn
+    
+
+
+  }
+}
+
+// console.log(throwDie())
+// console.log(throwDie())
+// console.log(throwDie())
+// console.log(throwDie())
+// console.log(throwDie())
