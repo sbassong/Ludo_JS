5d8c28a008cd76661e8be1433aaf8f6d1c497abe
Sam Bassong <sam.bassong@gmail.com>
2021-08-09 17:38:26 -0700

    still working on movePawn function, also added some more css to give shape to the board

--- script.js
+++ script.js
@@ -3,52 +3,63 @@ const redStartPoint = document.querySelector('#c1')
 const greenStartPoint = document.querySelector('#c14')
 const yellowStartPoint = document.querySelector('#c27')
 const blueStartPoint = document.querySelector('#c40')
-let cell = document.querySelector(`.${}${}`)
-let startsFrom = ''
+// let cell = document.querySelector(`.${}${}`)
+
 
 
 //Die functionality
 // const dieDiv = document.querySelector('.die')
-const throwDie = () => Math.ceil(Math.random() * 6)
 let dieVal = 0
+const throwDie = () => {
+  dieVal = Math.ceil(Math.random() * 6)
+}
 
 
 //pawn class and methods/behavior
-class Pawn  {
-  constructor (color) {
-    this.color = color
-  }
+// class Pawn  {
+//   constructor (color) {
+//     this.color = color
+//   }
 
-  startsFrom(target) {
-    if (target.id.parentElement.id.StartsWith('red-')) {
-      target.parentElement.removeChild(target)
+  const startsFrom = (e) => {
+    let target = e.target
+    console.log(target.id)
+    let parent = target.parentElement
+    console.log(parent)
+
+    if (target.parent.id.startsWith('red-')) {
+      parent.removeChild(target)
       redStartingPoint.appendChild(target)
-    } else if (target.id.parentElement.id.StartsWith('green-')) {
-      target.parentElement.removeChild(target)
+    } else if (target.parent.id.startsWith('green-')) {
+      parent.removeChild(target)
       greenStartingPoint.appendChild(target)
-    } else if (target.id.parentElement.id.StartsWith('yellow-')) {
-      target.parentElement.removeChild(target)
+    } else if (target.parent.id.startsWith('yellow-')) {
+      parent.removeChild(target)
       yellowStartingPoint.appendChild(target)
     } else {
-      target.parentElement.removeChild(target)
+      parent.removeChild(target)
       blueStartingPoint.appendChild(target)
     }
   }
 
-  movePawn(e) {
-    let curPawn = e.target
-    if (curPawn.parentElement.id.startsWith())
-    startsFrom
+  // movePawn(e, dieVal) {
+  //   let curPawn = e.target
+  //   let parent = curPawn.parentElement
+  //   if (!curPawn.parent.id.startsWith('c')) { return }
+  //   else {
+  //     parent.removeChild(target)
+  //     let newParent = document.querySelector(`.${parent.id}`)
+  //   }
     
-    curPawn
-    
+  // }
+//}
+
+  const redPawn1 = document.getElementById('rhc1')
+redPawn1.style.backgroundColor = 'red'
+const redPawn2 = document.querySelector('#rhc2')
+const redPawn3 = document.querySelector('#rhc3')
+const redPawn4 = document.querySelector('#rhc4')
+
+redPawn1.addEventListener('click', startsFrom(e))
 
 
-  }
-}
-
-// console.log(throwDie())
-// console.log(throwDie())
-// console.log(throwDie())
-// console.log(throwDie())
-// console.log(throwDie())
